d7aee208e0305b50b6d9e17df2481ffa6e1a4ece
davlat200710 <140897179+davlat200710@users.noreply.github.com>
2023-10-07 15:22:32 +0300

    Add files via upload

---
+++ 8__/index.js
@@ -0,0 +1,331 @@
+// // // for(let i = 1; i <= 50; i++)[
+
+// // // ]
+
+
+
+// let n = prompt ("son krtelsn")
+// let m = 50
+
+// for(let i =1; i <=n;  i++){
+//         if(n % i == 0){
+//             console.log(i);
+//         }
+//     }
+
+
+// // // for(let i =1; i <= 50; i++){
+// // //     if(i % 6 == 0 && i % 2 == 0 && i % 3 == 0 && i % 5 )
+// // //       {  console.log(i);
+// // //     }
+// // // }
+
+
+// // // let i = 0;
+
+// // // while(i <= 50) {
+// // //     if(i % 2 == 0)
+// // //     console.log(i); 
+
+// // //     i++
+// // // }
+
+// // // let tech =[
+// // //     {
+// // //         id:1,
+// // //         name:'Iphone',
+// // //         price:'129',
+// // //         model:'Redm 5 +',
+// // //         year:2021
+// // //     },
+
+// // //     {
+// // //         id:2,
+// // //         name:'Xiaomi',
+// // //         price:'309',
+// // //         model:'Redm Not12 pro',
+// // //         year:2022
+// // //     },
+
+// // //     {
+// // //         id:3,
+// // //         name:'Huvaviy',
+// // //         price:'809',
+// // //         model:'Tran5parent 3.1',
+// // //         year:2020
+// // //     },
+
+// // //     {
+// // //         id:4,
+// // //         name:'Samsung',
+// // //         price:'590',
+// // //         model:'Samsung not 8',
+// // //         year:2021
+// // //     },
+   
+// // // ]
+// // // tech.map(telfon => {
+// // //     if(telfon.name == )
+// // // })
+
+
+
+// // // console.log(tech)
+
+
+// // let tech = [
+
+// //     {
+// //         name: "Redmi",
+// //         price: "3000",
+// //         model: "Redmi 6 pro max",
+// //         year: "2024",
+
+// //     },
+
+// //     {
+// //         name: "Redmi",
+// //         price: "1099",
+// //         model: "Redmi 9 sport",
+// //         year: "2000",
+
+// //     },
+
+// //     {
+// //         name: "Iphone",
+// //         price: "2000",
+// //         model: "Iphone 6 pro max",
+// //         year: "2020",
+
+// //     },
+
+// //     {
+// //         name: "Samsung",
+// //         price: "2090",
+// //         model: "Samsung 22 ultra",
+// //         year: "2019",
+
+// //     },
+
+// // ]
+
+// // tech.map(item => {
+    
+// //         console.log(item.name);
+    
+// // })
+
+// let   posts = [
+//         {
+//             "postId": 1,
+//             "id": 1,
+//             "name": "id labore ex et quam laborum",
+//             "email": "Eliseo@gardner.biz",
+//             "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium",
+//             "date": "January",
+//             "year": 2015,
+//             "count": 33
+//           },
+//           {
+//             "postId": 4,
+//             "id": 2,
+//             "name": "quo vero reiciendis velit similique earum",
+//             "email": "Jayne_Kuhic@sydney.com",
+//             "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+//             "date": "March",
+//             "year": 2018,
+//             "count": 49
+//           },
+//           {
+//             "postId": 3,
+//             "id": 3,
+//             "name": "odio adipisci rerum aut animi",
+//             "email": "Nikita@garfield.biz",
+//             "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione",
+//             "date": "December",
+//             "year": 2020,
+//             "count": 10
+//           },
+//           {
+//             "postId": 3,
+//             "id": 4,
+//             "name": "alias odio sit",
+//             "email": "Lew@alysha.tv",
+//             "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati",
+//             "date": "October",
+//             "year": 2010,
+//             "count": 105
+//           },
+//           {
+//             "postId": 4,
+//             "id": 5,
+//             "name": "vero eaque aliquid doloribus et culpa",
+//             "email": "Hayden@althea.biz",
+//             "body": "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et",
+//             "date": "December",
+//             "year": 2019,
+//             "count": 88,
+//           },
+//     ]
+
+    
+
+
+//     posts.map( item => {
+//         if(item.count){
+//             console.log(item.email);
+//         }
+//     })
+
+
+
+// posts.map(item => {
+//     if(item.postId == 4){
+//       console.log(item.count);
+//     }
+//   })
+
+// posts.map(item => {
+//     if(item.postId == 3){
+//       console.log(item.count);
+//     }
+//   })
+
+
+
+
+// console.log("0.1 " +0.1);
+
+
+
+
+
+
+// const points = [40, 100, 1, 5, 25, 10, 523, 21, 654,11111, ];
+// points.sort(function(a, b){return b - a});
+// console.log(points);
+
+// function myArrayMin(arr) {
+//     let len = arr.length;
+//     let min = Infinity;
+//     while (len--) {
+//       if (arr[len] < min) {
+//         min = arr[len];
+//       }
+//     }
+//     return min;
+//   }
+//   console.log();
+
+
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Mirjalol", "zarshid", "Davlatjon",];
+
+// fruits.splice(6 ,1);
+// console.log(fruits  )
+
+// let numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// numberArr.splice(6 ,1)
+
+// console.log(numberArr);
+
+
+
+
+// let numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// numberArr.splice(6 ,1)
+
+// console.log(numberArr);
+
+// let numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// numberArr.reverse()
+
+// console.log(numberArr);
+
+// let numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// numberArr.splice(9 , 1 , "salom")
+
+// console.log(numberArr);
+
+// let random = Math.floor(Math.random() * 100);
+
+// console.log(random);
+
+
+// function clicMe (){
+// let a = prompt("Ismingizn kriting")
+// console.log("Foydalanuvchi ism", a);
+// }
+
+
+
+
+// let moud = false
+// const darkModeFunc = () =>{
+    // moud == !moud
+    
+    
+    // console.log(moud);
+    
+    // if(moud = true){
+        //     document.bodey.style.backgroundColor = "black"
+        // }
+        // }
+        
+        //  const colorFunc = () => {
+            //     document.body.style.backgroundColor = prompt("Rangni kiriting")
+            // }
+            
+         
+
+           
+
+//             function clicMe (){
+//             let a = prompt("Ismingizn kriting")
+//             console.log("Foydalanuvchi ism", a);
+//             }
+
+//         let count =16
+
+// const incrementText = () => {
+//     count = count + 2;
+//     document.body.style.fontSize = count + "px";
+    
+// }
+
+
+
+// const decrementText = () => {
+//     if (count > 10){
+//         count = count - 2;
+//         document.body.style.fontSize = count + "px";
+//     }
+// }
+
+// let mode = true
+
+// const darkmodefun = ()=>{
+//    if (mode ==true){
+//     document.body.style.backgroundColor = "black"
+//     document.body.style.color ="white"
+//    }else{
+//     document.body.style.backgroundColor = "white"
+//     document.body.style.color ="black"
+//    }
+
+//    console.log(mode);
+//    mode = !mode
+
+// }
+
+
+// const colorFunc = () => {
+//     document.body.style.backgroundColor = prompt("Rangni kiriting")
+// }
+
+// const shriftFunc = () => {
+//     document.body.style.fontSize = prompt("Shriftni kiriting")
+// }
+
+
